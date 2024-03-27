4d645e69fac1573d97a2ba2099123b65c89f39af
Emilio Cliff <emiliocliff@gmail.com>
2024-03-27 20:07:26 +0300

    changed twilio apikeys

--- weather_forecast/main.py
+++ weather_forecast/main.py
@@ -4,10 +4,10 @@ from twilio.rest import Client
 # lat={lat}&lon={lon}&exclude={part}&appid={API key}
 MY_LAT = -0.023559
 MY_LONG = 37.906193
-account_sid = "ACd6d10ee3b285cb836efde62b95928230"
-auth_token = "929af8e509a3d601855b62819224c30c"
-twilio_number = "+12569077962"
-my_api_key = "f17eab2854fd8ac1eb02db51e2bf2005"
+account_sid = "twilio_account_sid"
+auth_token = "twilio_auth_token"
+twilio_number = "twilio_number"
+my_api_key = "twilio_api_key"
 parameters = {
     "q": "Nairobi",
     # "lat": MY_LAT,
